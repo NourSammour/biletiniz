8f78d2cdb5139a15f41e9b1c0d7e9e2e6c151e3c
AbdilFattahBoshi <AbdilFattahBoshi001997@gmail.com>
2019-07-21 18:44:15 +0300

    fix conflicts

---
+++ .next/static/development/pages/day.js
@@ -0,0 +1,86 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\day.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fday&absolutePagePath=H%3A%5CProjects%5Cbiletiniz%5Cpages%5Cday%5Cindex.js!./":
+/*!********************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fday&absolutePagePath=H%3A%5CProjects%5Cbiletiniz%5Cpages%5Cday%5Cindex.js ***!
+  \********************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/day", function() {
+      var page = __webpack_require__(/*! ./pages/day/index.js */ "./pages/day/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/day/index.js */ "./pages/day/index.js", function() {
+          if(!next.router.components["/day"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/day/index.js */ "./pages/day/index.js")
+          next.router.update("/day", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_7aff549c98b978433226 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_7aff549c98b978433226 */ "dll-reference dll_7aff549c98b978433226"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/day/index.js":
+/*!****************************!*\
+  !*** ./pages/day/index.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "H:\\Projects\\biletiniz\\pages\\day\\index.js";
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 1
+    },
+    __self: this
+  }, "Hello Day");
+});
+
+/***/ }),
+
+/***/ 1:
+/*!************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fday&absolutePagePath=H%3A%5CProjects%5Cbiletiniz%5Cpages%5Cday%5Cindex.js ***!
+  \************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fday&absolutePagePath=H%3A%5CProjects%5Cbiletiniz%5Cpages%5Cday%5Cindex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fday&absolutePagePath=H%3A%5CProjects%5Cbiletiniz%5Cpages%5Cday%5Cindex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_7aff549c98b978433226":
+/*!*******************************************!*\
+  !*** external "dll_7aff549c98b978433226" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_7aff549c98b978433226;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=day.js.map
