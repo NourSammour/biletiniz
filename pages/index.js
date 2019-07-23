b6a803cd64942ca5aeb6c85fd43bd9ad9312196d
LamaDabbeet <lamadabbeet@gmail.com>
2019-07-23 12:19:53 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,26 @@
-export default () => <div>{process.env.TEST}</div>
+import React from 'react'
+import Link from 'next/link'
 
-
+export default () => (
+  <ul>
+    <li>
+      <Link href='/b' as='/a'>
+        <a>a</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/a' as='/b'>
+        <a>b</a>
+      </Link>
+    </li>
+    <li>
+      <Link href={{ pathname: '/posts', query: { id: '2' } }} as='/posts/2'>
+        <a>post #2</a>
+      </Link>
+    </li>
+    <li>
+     <div>{process.env.TEST}</div>
+    </li>
+  </ul>
+)
 
