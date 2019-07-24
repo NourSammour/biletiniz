d2efcb73a3db92f00d97473f143f5a18c5b7cf2d
AbdilFattahBoshi <AbdilFattahBoshi001997@gmail.com>
2019-07-25 01:21:19 +0300

    Add button component to storybook

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
+import styled, { css } from 'styled-components';
+
+const CustomButton = styled.button`
+background: #FAFAFABB;
+border-radius: 5px;
+border: 0px;
+color: #03a9f4;
+margin: 0 1em;
+padding: 0.25em 1em;
+${props =>
+  props.primary &&
+  css`
+    background: #03a9f455;
+    border: 0px;
+    color: #FAFAFA;
+  `};
+`;
+
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -8,4 +26,13 @@ storiesOf('Button', module)
   ))
   .add('with emoji', () => (
     <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+  ))
+  .add('with another emoji', () => (
+    <Button><span role="img" aria-label="so cool">😉😢😁😊😍😎</span></Button>
+  ))
+  .add('Custom Button',()=>(
+    <CustomButton>Custom Button</CustomButton>
+  ))
+  .add('Custom Button primary',()=>(
+    <CustomButton primary>Custom Button primary</CustomButton>
   ));
