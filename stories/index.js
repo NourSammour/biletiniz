16ea635ba9f474d7e0aea6f82d05e2049003489a
MohammadSommakia <mohammadsommakia95@gmail.com>
2019-07-25 16:32:05 +0300

    test

--- stories/index.js
+++ stories/index.js
@@ -4,35 +4,38 @@ import { Button } from '@storybook/react/demo';
 import styled, { css } from 'styled-components';
 
 const CustomButton = styled.button`
-background: #FAFAFABB;
-border-radius: 5px;
-border: 0px;
-color: #03a9f4;
-margin: 0 1em;
-padding: 0.25em 1em;
-${props =>
-  props.primary &&
-  css`
-    background: #03a9f455;
-    border: 0px;
-    color: #FAFAFA;
-  `};
+  background: #fafafabb;
+  border-radius: 5px;
+  border: 0px;
+  color: #03a9f4;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  ${props =>
+    props.primary &&
+    css`
+      background: #03a9f455;
+      border: 0px;
+      color: #fafafa;
+    `};
 `;
 
-
 storiesOf('Button', module)
-  .add('with text', () => (
-    <Button>Hello Button</Button>
-  ))
+  .add('with text', () => <Button>Hello Button</Button>)
   .add('with emoji', () => (
-    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+    <Button>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
   ))
   .add('with another emoji', () => (
-    <Button><span role="img" aria-label="so cool">😉😢😁😊😍😎</span></Button>
+    <Button>
+      <span role="img" aria-label="so cool">
+        😉😢😁😊😍😎
+      </span>
+    </Button>
   ))
-  .add('Custom Button',()=>(
-    <CustomButton>Custom Button</CustomButton>
-  ))
-  .add('Custom Button primary',()=>(
+  .add('Custom Button', () => <CustomButton>Custom Button</CustomButton>)
+  .add('Custom Button primary', () => (
     <CustomButton primary>Custom Button primary</CustomButton>
-   ));
+  ));
